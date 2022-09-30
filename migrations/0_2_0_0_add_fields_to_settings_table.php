1f3becf141a931f04f4481d8c2cdaecef15748bb
Robert <rtudor@palmcentre.co.uk>
2022-09-30 13:20:57 +0100

    added predefined values in database

---
+++ migrations/0_2_0_0_add_fields_to_settings_table.php
@@ -0,0 +1,22 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSettingsTable extends Migration
+{
+    public function up()
+    {
+        Schema::table('settings', function (Blueprint $table) {
+          $table->json('values');
+          $tble->string('default');
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('settings', function (Blueprint $table) {
+            $table->dropColumn('values', 'default');
+        });
+    }
+}
